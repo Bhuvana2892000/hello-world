acdae18ae30708c027eb6c5b057e576bafd3cf66
bhuvana <bhuvi2892000@gmail.com>
2021-10-05 19:23:57 +0530

    commit

---
+++ login/login.php
@@ -0,0 +1,28 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "login";
+
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+$email = $_POST['email'];
+
+
+
+$stmt = $conn->prepare("select * from user ($usename,md5($password),$email) VALUES (?, ?, ?)");
+
+if($conn) {
+        echo "successfully login"; 
+    } 
+    else {
+        die("Error". mysqli_connect_error()); 
+    } 
+
+$stmt->close();
+$conn->close();
+?>
---
+++ signup/signup.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "signup";
+
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+$email = $_POST['email'];
+
+
+$stmt = $conn->prepare("INSERT INTO user ($usename,md5($password),$email) VALUES (?, ?, ?)");
+
+if($conn) {
+        echo "successfull signup"; 
+    } 
+    else {
+        die("Error". mysqli_connect_error()); 
+    } 
+$stmt->close();
+$conn->close();
+
+?>
