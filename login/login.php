06d37d09cd7982c7b78a24cafba261134d80bcee
bhuvana <bhuvi2892000@gmail.com>
2021-10-20 11:27:56 +0530

    commit

--- login/login.php
+++ login/login.php
@@ -1,18 +1,23 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "php";
-
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if($conn) {
-        error_log("Failed to connect to database!", 0);
-    } 
-    else {
-        error_log("Successfully connected", 1);
-    } 
+class DB {
+    protected $servername = "localhost";
+    protected $username = "root";
+    protected $password = "";
+    protected $dbname = "php";
+	
+	public function connect() {
+	
+		$connect_db = new mysqli( $this->servername, $this->username, $this->password, $this->dbname );
+		
+		if($connect_db) {
+            error_log("Failed to connect to database!", 0);
+        } 
+        else {
+            error_log("Successfully connected", 1);
+        } 
+		
+	}
+}
 
 
 $username = $_POST['username'];
